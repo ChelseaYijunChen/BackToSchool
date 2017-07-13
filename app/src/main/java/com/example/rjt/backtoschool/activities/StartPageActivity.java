6c25d2cfa68e55d0348c136305e99eb39def698c
Yijun Chen <yijun2@g.clemson.edu>
2017-07-13 09:24:43 -0500

    login page

--- app/src/main/java/com/example/rjt/backtoschool/activities/StartPageActivity.java
+++ app/src/main/java/com/example/rjt/backtoschool/activities/StartPageActivity.java
@@ -1,15 +1,28 @@
 package com.example.rjt.backtoschool.activities;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.rjt.backtoschool.R;
+import com.example.rjt.backtoschool.fragments.SignInFragment;
 
-public class StartPageActivity extends AppCompatActivity {
+public class StartPageActivity extends AppCompatActivity implements SignInFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+        // Add the fragment to the 'fragment_container' FrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.startPage_fragment_container, new SignInFragment()).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
