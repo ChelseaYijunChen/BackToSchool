39b47261b988a6a06fa4c2ce050f683216b5a3b7
Yijun Chen <yijun2@g.clemson.edu>
2017-07-17 03:13:41 -0500

    add google map

--- app/src/main/java/com/example/rjt/backtoschool/activities/MainActivity.java
+++ app/src/main/java/com/example/rjt/backtoschool/activities/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         MapFragment mapFragment = MapFragment.newInstance();
         mainPageFragment = new MainPageFragment();
         //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, absentStudentFragment).commit();
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mapFragment).commit();
+        //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mapFragment).commit();
        // getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, allStudentsFragment).commit();
         //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, studentAttendentDetailsFragment).commit();
         getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mainPageFragment).commit();
