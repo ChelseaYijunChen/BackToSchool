e4ac1a83e39ff9d01461072256b20b50f66eaf02
Yijun Chen <yijun2@g.clemson.edu>
2017-07-18 15:46:35 -0500

    navigation view added

--- app/src/main/java/com/example/rjt/backtoschool/activities/MainActivity.java
+++ app/src/main/java/com/example/rjt/backtoschool/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.rjt.backtoschool.activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.rjt.backtoschool.R;
 import com.example.rjt.backtoschool.fragments.AbsentStudentFragment;
@@ -10,17 +15,29 @@ import com.example.rjt.backtoschool.fragments.AllStudentsFragment;
 import com.example.rjt.backtoschool.fragments.MainPageFragment;
 import com.example.rjt.backtoschool.fragments.MapFragment;
 import com.example.rjt.backtoschool.fragments.StudentAttendentDetailsFragment;
-import com.example.rjt.backtoschool.models.MainPageItem;
 
 public class MainActivity extends AppCompatActivity {
     AbsentStudentFragment absentStudentFragment;
     AllStudentsFragment allStudentsFragment;
     StudentAttendentDetailsFragment studentAttendentDetailsFragment;
     MainPageFragment mainPageFragment;
+
+    private ActionBarDrawerToggle toggle;
+    DrawerLayout drawerlayout;
+    NavigationView navigationview;
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        hideScrollBar();
+        setActionBar();
+        setDrawerToggle();
+        setListener();
+
         absentStudentFragment = new AbsentStudentFragment();
         allStudentsFragment = new AllStudentsFragment();
         studentAttendentDetailsFragment = new StudentAttendentDetailsFragment();
@@ -28,14 +45,51 @@ public class MainActivity extends AppCompatActivity {
         mainPageFragment = new MainPageFragment();
         //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, absentStudentFragment).commit();
         getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mapFragment).commit();
-        //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, allStudentsFragment).commit();
-        //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, studentAttendentDetailsFragment).commit();
-        //getSupportFragmentManager().beginTransaction().replace(R.id.mainContent, mainPageFragment).commit();
 
     }
 
+    private void initViews() {
+        this.drawerlayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        this.navigationview = (NavigationView) findViewById(R.id.navigation_view);
+        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
 
+    private void hideScrollBar() {
+        navigationview.getChildAt(0).setVerticalScrollBarEnabled(false);
+    }
 
+    private void setActionBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    private void setDrawerToggle() {
+        toggle = new ActionBarDrawerToggle(this, drawerlayout, toolbar, 0, 0);
+        drawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
 
+    private void setListener() {
+        navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.absent_student:
+                        break;
+                    case R.id.student_info:
+                        break;
+                    case R.id.bus_tracking:
+                        break;
+                    case R.id.school_info:
+                        break;
+                    case R.id.driver_info:
+                        break;
+                    case R.id.birthday:
+                        break;
+                }
+                drawerlayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/rjt/backtoschool/fragments/MapFragment.java
+++ app/src/main/java/com/example/rjt/backtoschool/fragments/MapFragment.java
@@ -1,6 +1,8 @@
 package com.example.rjt.backtoschool.fragments;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.rjt.backtoschool.R;
+import com.example.rjt.backtoschool.controllers.DataParser;
 import com.example.rjt.backtoschool.models.pojo.BusResponse;
 import com.example.rjt.backtoschool.models.pojo.UpdatedBusLocationItem;
 import com.example.rjt.backtoschool.rest.ApiClient;
@@ -24,10 +27,24 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import okhttp3.Route;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -36,15 +53,14 @@ import retrofit2.Response;
  * Created by yijunchen on 7/16/17.
  */
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMarkerClickListener{
+public class MapFragment extends Fragment implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMarkerClickListener {
     private GoogleMap mMap;
     Context context;
     private final static String ROUTE_ID = "101";
     List<UpdatedBusLocationItem> locationList = new ArrayList<>();
-    String lat = null;
-    String loni = null;
+    Double lat = null;
+    Double longi = null;
     private final String TAG = MapFragment.class.getSimpleName();
-    //private static final String TAG = .getSimpleName();
 
     public static MapFragment newInstance() {
         MapFragment mapFragment = new MapFragment();
@@ -59,10 +75,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Activit
         call.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
-                Log.d(TAG,response.code()+"");
+                Log.d(TAG, response.code() + "");
                 BusResponse busResponse = (BusResponse) response.body();
                 locationList = busResponse.getUpdatedBusLocation();
-                Log.d(TAG,locationList.get(0).toString());
+                Log.d(TAG, locationList.get(0).toString());
             }
 
             @Override
@@ -89,24 +105,37 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Activit
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng school = new LatLng(12.416514, 77.504332);
+        LatLng bus = new LatLng(0, 0);
 
-        for(int i =0; i< locationList.size();i++){
-            double lat = Double.parseDouble(locationList.get(i).getUpdatedLattitude());
-            double longi = Double.parseDouble(locationList.get(i).getUpdatedLongtitude());
+
+        for (int i = 0; i < locationList.size(); i++) {
+            lat = Double.parseDouble(locationList.get(i).getUpdatedLattitude());
+            longi = Double.parseDouble(locationList.get(i).getUpdatedLongtitude());
+            bus = new LatLng(lat, longi);
             Marker marker_car = mMap.addMarker(new MarkerOptions()
                     .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_car_48))
                     .title("24 Km/h")
-                    .position(new LatLng(lat, longi)));
+                    .position(bus));
         }
-
-//        Marker marker_car = mMap.addMarker(new MarkerOptions()
-//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_car_48))
-//                .title("24 Km/h")
-//                .position(new LatLng(41.838874, -87.857666)));
 
         Marker marker_school = mMap.addMarker(new MarkerOptions()
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_school_48))
-                .position(new LatLng(12.416514, 77.504332)));
+                .position(school));
+
+//
+//        String encodedString = overviewPolylines.getString("points");
+//        List<LatLng> list = decodePoly(encodedString);
+//        Polyline line = mMap.addPolyline(new PolylineOptions()
+//                .add(school, bus)
+//                .width(10)
+//                .color(Color.parseColor("#05b1fb"))
+//                .geodesic(true));
+
+        FetchUrl FetchUrl = new FetchUrl();
+
+        // Start downloading json data from Google Directions API
+        FetchUrl.execute(makeURL(12.416514, 77.504332,lat,longi));
 
         //marker_car.setTag(0);
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(40.000, 40.000)));
@@ -114,6 +143,263 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Activit
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(12.716514, 77.404332), 9));
 
+    }
+
+    // Fetches data from url passed
+    private class FetchUrl extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try {
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+                Log.d("Background Task data", data.toString());
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+
+        }
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+            Log.d("downloadUrl", data.toString());
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                Log.d("ParserTask",jsonData[0].toString());
+                DataParser parser = new DataParser();
+                Log.d("ParserTask", parser.toString());
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+                Log.d("ParserTask","Executing routes");
+                Log.d("ParserTask",routes.toString());
+
+            } catch (Exception e) {
+                Log.d("ParserTask",e.toString());
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points;
+            PolylineOptions lineOptions = null;
+
+            // Traversing through all the routes
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(10);
+                lineOptions.color(Color.RED);
+
+                Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            if(lineOptions != null) {
+                mMap.addPolyline(lineOptions);
+            }
+            else {
+                Log.d("onPostExecute","without Polylines drawn");
+            }
+        }
+    }
+
+    public String makeURL (double sourcelat, double sourcelog, double destlat, double destlog ){
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("http://maps.googleapis.com/maps/api/directions/json");
+        urlString.append("?origin=");// from
+        urlString.append(Double.toString(sourcelat));
+        urlString.append(",");
+        urlString
+                .append(Double.toString( sourcelog));
+        urlString.append("&destination=");// to
+        urlString
+                .append(Double.toString( destlat));
+        urlString.append(",");
+        urlString.append(Double.toString( destlog));
+        urlString.append("&sensor=false&mode=driving&alternatives=true");
+        urlString.append("&key=AIzaSyALi2ZeTaHn4DWxn7azv22oBOKKWT0RFis");
+        return urlString.toString();
+    }
+
+    public void drawPath(String  result) {
+
+        try {
+            //Tranform the string into a json object
+            final JSONObject json = new JSONObject(result);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            List<LatLng> list = decodePoly(encodedString);
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .addAll(list)
+                    .width(12)
+                    .color(Color.parseColor("#05b1fb"))//Google maps blue color
+                    .geodesic(true)
+            );
+
+        }
+        catch (JSONException e) {
+
+        }
+    }
+
+    private List<LatLng> decodePoly(String encoded) {
+
+        List<LatLng> poly = new ArrayList<LatLng>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+
+    protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+        ArrayList<LatLng> points;
+        PolylineOptions lineOptions = null;
+
+        // Traversing through all the routes
+        for (int i = 0; i < result.size(); i++) {
+            points = new ArrayList<>();
+            lineOptions = new PolylineOptions();
+
+            // Fetching i-th route
+            List<HashMap<String, String>> path = result.get(i);
+
+            // Fetching all the points in i-th route
+            for (int j = 0; j < path.size(); j++) {
+                HashMap<String, String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                points.add(position);
+            }
+
+            // Adding all the points in the route to LineOptions
+            lineOptions.addAll(points);
+            lineOptions.width(10);
+            lineOptions.color(Color.RED);
+
+            Log.d("onPostExecute","onPostExecute lineoptions decoded");
+
+        }
+
+        // Drawing polyline in the Google Map for the i-th route
+        if(lineOptions != null) {
+            mMap.addPolyline(lineOptions);
+        }
+        else {
+            Log.d("onPostExecute","without Polylines drawn");
+        }
     }
 
     @Override
